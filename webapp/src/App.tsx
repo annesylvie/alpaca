9155041f74a81d63b81902f9eecc78daefb6e1ed
Anne-Sylvie Deutsch <annesylviedeutsch@gmail.com>
2022-01-10 22:35:55 +0000

    webapp: first mock App

---
+++ webapp/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <button className='p-2 rounded mx-20 hover:bg-red-600 hover:text-white' >
+          {  "Click me!"}
+      </button>
+    </div>
+
+  );
+}
+
+export default App;
