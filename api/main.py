d742944344f5c9dd63d901cde232f654338a889c
Anne-Sylvie Deutsch <annesylviedeutsch@gmail.com>
2022-01-13 22:27:42 +0000

    api: basic service for test

--- api/main.py
+++ api/main.py
@@ -1,9 +1,24 @@
-from bottle import route, run, template
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+    "http://localhost:3000",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
-@route("/hello/<name>")
-def index(name):
-    return template("<b>Hello {{name}}</b>!", name=name)
-
-
-run(host="localhost", port=8080)
+@app.get("/hello/{name}")
+async def root(name: str):
+    return {"message": name}
