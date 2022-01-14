0bcaf70e0b355b3aedde3f3942b2b869c19c4a52
Anne-Sylvie Deutsch <annesylvie@reinfer.io>
2022-01-14 11:38:49 +0000

    webapp: refactor Box and Form into their own files

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,129 +1,28 @@
 import React, { useState, useEffect, Dispatch, SetStateAction } from "react";
 import "./App.css";
+import { Box } from "./Box";
+import { FormCreator } from "./Form";
 
 function App() {
   const [boxes, setBoxes] = useState<Array<string>>([]);
   const [showFields, setShowFields] = useState(false);
 
   return (
-    <div className="bg-gray-200 rounded-lg m-4 p-4">
-      {boxes.map((box) => (
-        <Box fieldName={box} />
-      ))}
-      <FormCreator
-        showFields={showFields}
-        setShowFields={setShowFields}
-        boxes={boxes}
-        setBoxes={setBoxes}
-      />
+    <div className="grid place-items-center">
+      <div className="bg-gray-200 rounded-lg m-4 p-4 ">
+        {boxes.map((box) => (
+          <Box fieldName={box} />
+        ))}
+        <FormCreator
+          showFields={showFields}
+          setShowFields={setShowFields}
+          boxes={boxes}
+          setBoxes={setBoxes}
+        />
+      </div>
     </div>
   );
 }
-
-function Box(props: { fieldName: string }) {
-  return (
-    <div className="text-white bg-gradient-to-r from-green-400 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm text-center my-2 p-4">
-      {" "}
-      Name: {props.fieldName}{" "}
-    </div>
-  );
-}
-
-export const useForm = (callback: any, initialState: { user: string }) => {
-  const [values, setValues] = useState(initialState);
-
-  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setValues({ ...values, [event.target.name]: event.target.value });
-  };
-
-  const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    await callback();
-  };
-
-  return {
-    onChange,
-    onSubmit,
-    values,
-  };
-};
-
-function Form(props: {
-  setShowFields: Dispatch<SetStateAction<boolean>>;
-  boxes: Array<string>;
-  setBoxes: Dispatch<SetStateAction<Array<string>>>;
-}) {
-  const initialState = { user: "" };
-  const { onChange, onSubmit, values } = useForm(addBoxCallback, initialState);
-
-  async function addBoxCallback() {
-    props.setShowFields(false);
-    props.setBoxes((boxes) => [...boxes, values["user"]]);
-  }
-
-  return (
-    <div className="w-full max-w-xs">
-      <form
-        onSubmit={onSubmit}
-        className="bg-white shadow-md rounded px-8 py-6 mb-4"
-      >
-        <div>
-          <label className="block text-gray-700 text-sm font-bold my-2">
-            Input
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full my-2 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="username"
-            placeholder="Value"
-            name="user"
-            onChange={onChange}
-            required
-          />
-        </div>
-        <div className="flex items-center">
-          <button
-            type="submit"
-            className="text-white bg-blue-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center my-2"
-          >
-            Submit
-          </button>
-        </div>
-      </form>
-    </div>
-  );
-}
-
-interface IFormProps {
-  showFields: boolean;
-  setShowFields: Dispatch<SetStateAction<boolean>>;
-  boxes: Array<string>;
-  setBoxes: Dispatch<SetStateAction<Array<string>>>;
-}
-
-export const FormCreator: React.FC<IFormProps> = ({
-  showFields,
-  setShowFields,
-  boxes,
-  setBoxes,
-}: IFormProps) => {
-  return (
-    <div className="flex justify-center">
-      {showFields ? (
-        <Form setShowFields={setShowFields} boxes={boxes} setBoxes={setBoxes} />
-      ) : (
-        <div>
-          <button
-            type="submit"
-            className="text-white bg-blue-700 font-medium rounded-lg text-sm text-center p-4"
-            onClick={() => setShowFields(true)}
-          >
-            Add New
-          </button>
-        </div>
-      )}
-    </div>
-  );
-};
 
 function MyComponent() {
   const [error, setError] = useState<{ message: string } | null>(null);
